23147b1218e4903e4b5900210464fd02cc9150dc
chenzhen <czhttp@gmail.com>
2014-04-29 19:42:18 +0800

    first checkin

---
+++ jmem.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+PID=$1;
+SML=$2;
+BIG=$3;
+DIR=/tmp;
+
+pmap -x ${PID} | awk "{if(\$3 > ${SML} && \$3 < ${BIG} ){START=\$1;ARG=\$3;system(\"gdb --quiet --batch --pid ${PID} -ex 'dump memory ${DIR}/${PID}-\"count\".dump 0x\"START\" 0x\"START\"+\"ARG\"';\");count++}}END{print count}"
