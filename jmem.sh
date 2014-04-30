5d7c67019fb94fe7e08a4aa521242df1102a3889
chenzhen <czhttp@gmail.com>
2014-04-30 16:47:48 +0800

    fix bug

--- jmem.sh
+++ jmem.sh
@@ -4,4 +4,4 @@ SML=$2;
 BIG=$3;
 DIR=/tmp;
 
-pmap -x ${PID} | awk "{if(\$3 > ${SML} && \$3 < ${BIG} ){START=\$1;ARG=\$3;system(\"gdb --quiet --batch --pid ${PID} -ex 'dump memory ${DIR}/${PID}-\"count\".dump 0x\"START\" 0x\"START\"+\"ARG\"';\");count++}}END{print count}"
+pmap -x ${PID} | awk "{if(\$3 > ${SML} && \$3 < ${BIG} ){START=\$1;ARG=\$3;system(\"gdb --quiet --batch --pid ${PID} -ex 'dump memory ${DIR}/${PID}-\"count\".dump 0x\"START\" 0x\"START\"+\"ARG\"001';\");count++}}END{print count}"
